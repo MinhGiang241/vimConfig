dbdf77ce65a2338ac07f63f0f8dd1dc73cbbe03d
Minh giang trương <minhgiang241@gmail.com>
2023-03-29 01:37:58 +0700

    add more dap ui

--- config.lua
+++ config.lua
@@ -99,6 +99,76 @@ lvim.autocommands = {
 }
 
 
+local dap = require "dap"
+local dapui = require "dapui"
+
+require("dapui").setup({
+  icons = { expanded = "▾", collapsed = "▸" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  -- Expand lines larger than the window
+  -- Requires >= 0.7
+  expand_lines = vim.fn.has("nvim-0.7"),
+  -- Layouts define sections of the screen to place windows.
+  -- The position can be "left", "right", "top" or "bottom".
+  -- The size specifies the height/width depending on position. It can be an Int
+  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+  -- Elements are the elements shown in the layout (in order).
+  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  layouts = {
+    {
+      elements = {
+        -- Elements can be strings or table with id and size keys.
+        { id = "scopes", size = 0.25 },
+        "breakpoints",
+        "stacks",
+        "watches",
+      },
+      size = 40, -- 40 columns
+      position = "left",
+    },
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 0.25, -- 25% of total lines
+      position = "bottom",
+    },
+  },
+  floating = {
+    max_height = nil,  -- These can be integers or a float between 0 and 1.
+    max_width = nil,   -- Floats will be treated as percentage of your screen.
+    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+  }
+})
+
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
 
 -- lvim.builtin.treesitter.ignore_install = { "haskell" }
 
@@ -159,6 +229,16 @@ lvim.plugins = {
   --
   --}
 
+  {
+    "folke/neodev.nvim",
+    config = function()
+      -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
+      require("neodev").setup({
+        -- add any options here, or leave empty to use the default settings
+        library = { plugins = { "nvim-dap-ui" }, types = true },
+      })
+    end
+  },
   {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
@@ -323,7 +403,14 @@ lvim.plugins = {
   {
     "theHamsta/nvim-dap-virtual-text",
     config = function()
-      require("nvim-dap-virtual-text").setup({})
+      require("nvim-dap-virtual-text").setup({
+        highlight_changed_variables = true,
+        highlight_new_as_changed = false,
+        all_frames = true,
+        only_first_definition = false,
+        all_references = true,
+        virt_text_win_col = 60,
+      })
     end
   },
   -- {
