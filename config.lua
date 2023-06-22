8032197f277c79ed01b527f85225e61bba63838a
Minh Giang <minhgiang241@gmail.com>
2023-06-22 13:38:51 +0700

    add scroll bar

--- config.lua
+++ config.lua
@@ -613,6 +613,20 @@ lvim.plugins = {
       vim.g.rnvimr_bw_enable = 1
     end,
   },
+  {
+    "dstein64/nvim-scrollview",
+    config = function()
+      require('scrollview').setup({
+        excluded_filetypes = { 'nerdtree' },
+        current_only = true,
+        winblend = 75,
+        base = 'buffer',
+        column = 80,
+        signs_on_startup = { 'all' },
+        diagnostics_severities = { vim.diagnostic.severity.ERROR }
+      })
+    end
+  }
 }
 
 --
