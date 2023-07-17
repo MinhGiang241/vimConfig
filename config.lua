e924aaa2766ab25824d1bf52046aeefd79edcc79
Minh Giang <minhgiang241@gmail.com>
2023-07-17 18:34:58 +0700

    Revert "add more folding code"
    This reverts commit 4183c213b6a2d0219ebcbb9ca1b60f6e28a94d25.

--- config.lua
+++ config.lua
@@ -46,7 +46,6 @@ lvim.format_on_save = {
 lvim.leader = "space"
 
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.builtin.which_key.setup.plugins.presets.z = true
 
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
