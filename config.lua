ea88dae34fefc7f0efd7e13aea158d93a6da3ea8
Minh Giang <minhgiang241@gmail.com>
2023-07-22 11:15:02 +0700

    add vim-go package

--- config.lua
+++ config.lua
@@ -283,6 +283,12 @@ lvim.plugins = {
   --       cmd = "TroubleToggle",
   --
   --}
+  {
+    "fatih/vim-go",
+    config = function()
+      require("vim-go").setup()
+    end
+  },
 
   {
     "folke/neodev.nvim",
@@ -448,11 +454,11 @@ lvim.plugins = {
           lsp = {
             color = {
               -- show the derived colours for dart variables
-              enabled = false,        -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-              background = false,     -- highlight the background
+              enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+              background = false, -- highlight the background
               background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-              foreground = false,     -- highlight the foreground
-              virtual_text = true,    -- show the highlight using virtual text
+              foreground = false, -- highlight the foreground
+              virtual_text = true, -- show the highlight using virtual text
               virtual_text_str = "■", -- the virtual text character to highlight
             },
           }
